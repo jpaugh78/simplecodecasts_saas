29f5a8b317ee3dc3215f3fb767044cbb9bd60e74
Joseph Paugh <jpaugh78@gmail.com>
2015-10-07 17:44:13 +0000

    Added profile model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
+  has_one :profile
   attr_accessor :stripe_card_token
   
   def save_with_payment
     if valid? #check validations on page
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
       self.stripe_customer_token = customer.id
-      flash[:notice] = "save payment info"
       save!
-      flash[:notice] = "save to db"
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006234410) do
+ActiveRecord::Schema.define(version: 20151007174107) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 20151006234410) do
   create_table "plans", force: true do |t|
     t.string  "name"
     t.decimal "price"
+  end
+
+  create_table "profiles", force: true do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.string   "phone_number"
+    t.string   "contact_email"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
