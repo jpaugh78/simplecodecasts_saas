b17379cd68b1202b4bebc1fb24a4653dde4dd381
Joseph Paugh <jpaugh78@gmail.com>
2015-10-08 16:23:18 +0000

    Updated Home page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -16,6 +16,11 @@ class ProfilesController < ApplicationController
        end
     end
     
+    def edit
+        @user = User.find( params[:user_id] ) 
+        @profile = @user.build_profile(profile_params)
+    end 
+    
     #whitelist what you need the user to submit
     private
         def profile_params
